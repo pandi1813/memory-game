f6a163573fce6e5649478c78decbb98dbecf85bf
pandi1813 <andrea.peter1813@gmail.com>
2023-06-13 15:40:49 +0100

    created function to randomize image array

--- src/components/GamePage/GamePage.tsx
+++ src/components/GamePage/GamePage.tsx
@@ -2,16 +2,23 @@ import ImageComponent from './ImageComponent'
 import './game-page.css'
 
 const GamePage = () => {
-  let imageArray: string[] = ["./images/chewbacca.jpeg", "./images/hansolo.jpg", "./images/kenobi.jpg", "./images/luke.jpg", "./images/Padme.jpeg", "./images/Princess_Leia.jpg", "./images/Qui-Gon_Jinn.png", "./images/vader.jpg", "./images/yoda.jpg"]
-  function randomizeImages(array:string[]) {
-    
+  let imageArray: string[] | number[] = ["./images/chewbacca.jpeg", "./images/hansolo.jpg", "./images/kenobi.jpg", "./images/luke.jpg", "./images/Padme.jpeg", "./images/Princess_Leia.jpg", "./images/Qui-Gon_Jinn.png", "./images/vader.jpg", "./images/yoda.jpg"]
+  function randomizeImages(array:string[] |number[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random()*(i+1));
+      [array[i], array[j]] = [array[j], array[i]]
+      
+    }
+    console.log(array)
+    return array
   }
-
+// let test:number[] | string[] = [1, 2, 3, 4, 5]
+  randomizeImages(imageArray)
   return (
     <>
       <h2>GamePage</h2>
       <section className='flex-container'>
-        {imageArray.map(image => <ImageComponent src={image}/>)}
+        {imageArray.map(image => <ImageComponent src={image} />)}
         
       </section>
     </>
